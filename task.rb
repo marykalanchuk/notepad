bad7032966493220a3330b0a746e3bca3ee1d2ec
marykalanchuk <mary.kalanchuk@gmail.com>
2023-03-14 10:06:20 +0300

    Version 1.0: Memo, Link and Task

--- task.rb
+++ task.rb
@@ -8,8 +8,20 @@ class Task < Post
   end
 
   def read_from_console
+    puts "Что надо сделать?"
+    @text = $stdin.gets.chomp
+
+    puts "К какому числу? Укажите дату в формате ДД.ММ.ГГГГ, например 13.03.2023"
+    input = $stdin.gets.chomp
+
+    @due_date = Date.parse(input)
   end
 
   def to_strings
+    time_string = "Создано: #{@created_at.strftime("%Y.%m.%d, %H:%M:%S")} \n\r \n\r"
+
+    deadline = "Крайний срок: #{@due_date}"
+
+    [deadline, @text, time_string]
   end
 end
